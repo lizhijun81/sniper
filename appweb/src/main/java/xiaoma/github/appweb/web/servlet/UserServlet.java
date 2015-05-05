9481fa284dbadc42082989ef796d65788cb0314c
chunxiao <915567926@qq.com>
2015-05-05 23:28:48 +0800

    2.通过自定义注解实现请求路径的分发(1)

--- appweb/src/main/java/xiaoma/github/appweb/web/servlet/UserServlet.java
+++ appweb/src/main/java/xiaoma/github/appweb/web/servlet/UserServlet.java
@@ -2,6 +2,9 @@ package xiaoma.github.appweb.web.servlet;
 
 import xiaoma.github.appweb.web.annotatioin.Servlet;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Created by Dell on 2015/5/5.
  */
@@ -9,7 +12,7 @@ import xiaoma.github.appweb.web.annotatioin.Servlet;
 public class UserServlet {
 
     @Servlet(value = "/index")
-    public String index() {
+    public String index(HttpServletRequest request, HttpServletResponse response) {
         return "/index";
     }
 }
