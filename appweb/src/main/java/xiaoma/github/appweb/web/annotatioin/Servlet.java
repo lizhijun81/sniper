add1492acfc355a4b0c7214369c1c38df67261aa
chunxiao <915567926@qq.com>
2015-05-05 23:27:35 +0800

    2.通过自定义注解实现请求路径的分发(1)

--- appweb/src/main/java/xiaoma/github/appweb/web/annotatioin/Servlet.java
+++ appweb/src/main/java/xiaoma/github/appweb/web/annotatioin/Servlet.java
@@ -1,7 +1,13 @@
 package xiaoma.github.appweb.web.annotatioin;
 
+import java.lang.annotation.*;
+
 /**
  * Created by Dell on 2015/5/5.
  */
+@Retention(value = RetentionPolicy.RUNTIME)
+@Target(value = {ElementType.TYPE, ElementType.METHOD})
+@Inherited
 public @interface Servlet {
+    String value() default "";
 }
--- appweb/src/main/java/xiaoma/github/appweb/web/servlet/UserServlet.java
+++ appweb/src/main/java/xiaoma/github/appweb/web/servlet/UserServlet.java
@@ -1,7 +1,15 @@
 package xiaoma.github.appweb.web.servlet;
 
+import xiaoma.github.appweb.web.annotatioin.Servlet;
+
 /**
  * Created by Dell on 2015/5/5.
  */
+@Servlet(value = "/user")
 public class UserServlet {
+
+    @Servlet(value = "/index")
+    public String index() {
+        return "/index";
+    }
 }
